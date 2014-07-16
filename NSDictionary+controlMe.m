b788606b443dbc6c986840889bbf1ffdd6517d1b
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-07-16 16:03:51 +0200

    First commit

---
+++ NSDictionary+controlMe.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+controlMe.m
+//  Cave Exclusive
+//
+//  Created by Benjamin SENECHAL on 16/07/2014.
+//  Copyright (c) 2014 Benjamin SENECHAL. All rights reserved.
+//
+
+#import "NSDictionary+controlMe.h"
+
+@implementation NSDictionary (controlMe)
+
+@end
